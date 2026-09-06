cec0d287be9508c175bc5cc0e1a9384146f9d787
mjau <mjaux3@proton.me>
2026-09-06 16:18:32 -0400

    initial particle analysis

--- morphology.py
+++ morphology.py
@@ -1,21 +1,39 @@
 import spacy
-from jamdict import Jamdict
+from jmdictpy import JMDict
 
 nlp = spacy.load("ja_core_news_sm")
 
-jam = Jamdict()
+jmd = JMDict()
 
 doc = nlp("私は猫です。ニャー！ 「お前、どこの子だ？」足に纏わりついてきたのは、小さな子猫だった。灰色の縞模様のふわふわした猫だ。 ")
 
 syntax = ""
 
+particles = {
+    "は" : "SUBJ ",
+}
+
+def dictionarycheck(word):
+    result = jmd.lookup(str(word))
+    for entry in result.entries:
+        sense = entry.senses[0]
+        text = str(sense.glosses[0])
+        p1 = text.find("'")
+        p2 = text.find("'", p1 + 1)
+        return text[p1 + 1:p2]
+
 for token in doc:
     if token.pos_ == "NOUN":
         print("ill try and translate")
-        result = jam.lookup(str(token))
-        syntax += str(result.entries[0])
+        result =  dictionarycheck(token)
+        syntax += str(result + " ")
+    elif token.pos_ == "PRON":
+        if str(token) == "私":
+            syntax += "I "
+    elif token.pos_ == "ADP":
+        syntax += particles.get(str(token), "idk ")
     else:
-        syntax += str(token.pos_)
+        syntax += str(token.pos_ + " ")
     print(token.pos_, end=" ")
     print(token.morph, end=" ")
 print("\n" + doc.text)
